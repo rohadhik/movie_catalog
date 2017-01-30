5c0091ae659dbf684884e53c23a4d4a9b7a11d44
rohadhik <rohit.adhikari24@gmail.com>
2017-01-30 15:36:14 +0530

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,16 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life","https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg","https://www.youtube.com/watch?v=SgoiKLFBA3Q")
+
+
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar", "A marine on an alien planet", "https://en.wikipedia.org/wiki/Avatar_(2009_film)#/media/File:Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+
+bajiraomastani = media.Movie("Baji Roa Mastani","A story of Baji Roa and his lover mastani", "https://en.wikipedia.org/wiki/Bajirao_Mastani#/media/File:Bajirao_Mastani_Poster_2.jpg","https://www.youtube.com/watch?v=QqTLr_hviPs")
+
+bajiraomastani.show_trailer()
